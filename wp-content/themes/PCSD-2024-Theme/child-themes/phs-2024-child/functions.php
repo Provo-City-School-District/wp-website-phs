42f81021af3d884850009d90531daee79f3e2bf1
Josh Espinoza <joshespi@gmail.com>
2024-07-01 12:41:43 -0600

    updated functions.php from parent

--- wp-content/themes/PCSD-2024-Theme/child-themes/phs-2024-child/functions.php
+++ wp-content/themes/PCSD-2024-Theme/child-themes/phs-2024-child/functions.php
@@ -1,6 +1,7 @@
 <?php
 
-function my_theme_variables() {
+function my_theme_variables()
+{
     $my_theme_variables = array(
         'logo' => get_stylesheet_directory_uri() . '/assets/img/site-logo.png',
         'full_school_name' => 'Provo High School',
@@ -13,12 +14,13 @@ function my_theme_variables() {
         'insta_link' => 'instatest',
         'facebook_link' => 'facebooktest',
         'twitter_link' => 'twittertest',
-        'full_calendar_link' => 'http://158.91.1.133:8082/school-calendar/'
+        'full_calendar_link' => 'https://provohigh.provo.edu/school-calendar/'
         // 'search_icon' => get_template_directory_uri() . '/assets/icons/search-loupe.svg'
     );
     return $my_theme_variables;
 }
-function pcsd_child_theme_enqueue_styles() {
+function pcsd_child_theme_enqueue_styles()
+{
     wp_enqueue_style('variables', get_stylesheet_directory_uri() . '/assets/css/variables.css', '', '1.0.0', false);
 }
 add_action('wp_enqueue_scripts', 'pcsd_child_theme_enqueue_styles', 9999);
