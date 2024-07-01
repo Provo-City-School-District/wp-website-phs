4fca52943946ac4be6b0fa8ffbc7d6acd2e8f20d
Josh Espinoza <joshespi@gmail.com>
2024-07-01 12:41:09 -0600

    updated sidebar-counseling.php from parent

--- wp-content/themes/PCSD-2024-Theme/child-themes/phs-2024-child/sidebar-counseling.php
+++ wp-content/themes/PCSD-2024-Theme/child-themes/phs-2024-child/sidebar-counseling.php
@@ -1,7 +1,7 @@
 <aside id="rightSidebar" class="rightSidebar">
-<a href="http://158.91.1.133:8082/teachers-staff/counseling-staff/">Find Your Counselor</a>
-<h2>Counseling Calendar</h2>
-<?php echo do_shortcode( '[calendar id="2293"]'); ?>
-<a href="https://safeut.org/"><img src="<?php echo get_template_directory_uri() . "/assets/icons/safeUTcrisisline.jpg" ?>" alt="Link to SafeUT" /></a>
+    <a href="https://provohigh.provo.edu/teachers-staff/counseling-staff/">Find Your Counselor</a>
+    <h2>Counseling Calendar</h2>
+    <?php echo do_shortcode('[calendar id="2293"]'); ?>
+    <a href="https://safeut.org/"><img src="<?php echo get_template_directory_uri() . "/assets/icons/safeUTcrisisline.jpg" ?>" alt="Link to SafeUT" /></a>
 
 </aside>
